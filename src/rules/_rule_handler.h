2558d2ac138a362da40db8100230110bb3d2b9e9
h1me01 <semih.oezalp@hak-feldkirch.at>
2025-05-16 18:08:44 +0200

    add board to rule

--- src/rules/_rule_handler.h
+++ src/rules/_rule_handler.h
@@ -1,17 +1,22 @@
 #pragma once
 
-#include "../region/region.h"
-#include "../region/CellIdx.h"
+#include "../defs.h"
 #include "../impact_map.h"
+#include "../region/CellIdx.h"
+#include "../region/region.h"
 
 namespace sudoku {
 struct RuleHandler {
+    RuleHandler(Board *board) : board_(board) {}
+    virtual ~RuleHandler() = default;
+
     virtual bool number_changed(CellIdx pos) = 0;
     virtual bool candidates_changed() = 0;
     virtual bool valid() = 0;
-    virtual void update_impact(ImpactMap& impact_map) = 0;
-    virtual void from_json(JSON& json) = 0;
+    virtual void update_impact(ImpactMap &impact_map) = 0;
+    virtual void from_json(JSON &json) = 0;
+
+protected:
+    Board *board_ = nullptr;
 };
-}
-
-
+} // namespace sudoku
