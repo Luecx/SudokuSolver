32a183ed3c6028e573b6d6aa948c1f89a1d7588c
DoPmarc <marc.eggers@icloud.com>
2025-06-10 20:36:26 +0200

    add email Styling

--- finnssudokusite/sudoku/views/contact.py
+++ finnssudokusite/sudoku/views/contact.py
@@ -1,4 +1,4 @@
-from django.core.mail import EmailMessage, send_mail
+from django.core.mail import EmailMessage, send_mail, EmailMultiAlternatives
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -31,18 +31,63 @@ def kontaktformular_view(request):
         admin_email.send()
 
         # 2. Auto-Antwort an den Absender
-        send_mail(
-            subject=_('Vielen Dank für deine Nachricht'),
-            message=_(
-                "Hello %(name)s,\n\n"
-                "Thank you for your message to SudokuSphere.\n"
-                "We will get back to you as soon as possible.\n\n"
-                "Best regards,\nFinn and the SudokuSphere Team"
-            ) % {'name': name},
+        #         send_mail(
+        #             subject=_('Vielen Dank für deine Nachricht'),
+        #             message=_(
+        #                 "Hello %(name)s,\n\n"
+        #                 "Thank you for your message to SudokuSphere.\n"
+        #                 "We will get back to you as soon as possible.\n\n"
+        #                 "Best regards,\nFinn and the SudokuSphere Team"
+        #             ) % {'name': name},
+        #
+        #             from_email='noreply@sudokusphere.com',
+        #             recipient_list=[user_email],
+        #         )
 
-            from_email='noreply@sudokusphere.com',
-            recipient_list=[user_email],
-        )
+        subject = _('Thank you for your message')
+        from_email = 'noreply@sudokusphere.com'
+        to = [user_email]
+
+        text_content = _(
+            "Hello %(name)s,\n\n"
+            "Thank you for your message to SudokuSphere.\n"
+            "We will get back to you as soon as possible.\n\n"
+            "Best regards,\n"
+            "Finn and the SudokuSphere Team"
+        ) % {'name': name}
+
+        html_content = _(
+            """
+            <p style="padding:2rem"></p>
+            <div style="
+                width: 100%%;
+                max-width: 600px;
+                min-width: 60%%;
+                box-sizing: border-box;
+                padding: 1rem 4vw 4rem;
+                margin: auto;
+                border: 1px solid #cccccc;
+                border-radius: 3rem;
+                background-color: white;
+                text-align: center;
+            ">
+                <img src="https://ndb.one/logo.png" alt="Logo" width="80" height="80" style="display: block; margin: 0 auto;" />
+                <p>SUDOKUSPHERE.COM</p>
+                <p style="text-align: left; margin-top: 6vh;">
+                    Hello %(name)s,<br><br>
+                    Thank you for your message to SudokuSphere.<br>
+                    We will get back to you as soon as possible.<br><br>
+                    Best regards,<br>
+                    Finn and the SudokuSphere Team
+                </p>
+            </div>
+            <p style="padding:5rem"></p>
+            """
+        ) % {'name': name}
+
+        msg = EmailMultiAlternatives(subject, text_content, from_email, to)
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
 
         return HttpResponse(_("Message sent successfully."))
 
