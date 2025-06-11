994f6b155ff49d97b900081fe6c65823cfd196d0
h1me01 <semih.oezalp@hak-feldkirch.at>
2025-06-11 12:50:59 +0200

    update requirements.txt

--- finnssudokusite/sudoku_site/settings.py
+++ finnssudokusite/sudoku_site/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 # === Authentication ===
